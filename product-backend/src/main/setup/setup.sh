f7187f2efc8fd7e53454aaae4334d4c39be4fae4
IlyasBaqqari <ilyas.baqqari@and.digital>
2022-09-15 11:29:36 +0100

    Updated setup scripts

--- product-backend/src/main/setup/setup.sh
+++ product-backend/src/main/setup/setup.sh
@@ -1,14 +1,1 @@
-# ~~~ Database Setup Script Instructions ~~~
-
-# To make this script executable,
-# navigate to the directory containing this file in the terminal, and run the following command:
-
-# chmod a+x setup.sh
-
-# Once this has been run,
-# run the following command in the same directory to create the database:
-
-# ./setup.sh
-
-# Make sure to input your own password --password=<here>
-mysql --user=root --password=mariadb.password < initialiseDatabase.sql
+mysql --user=root --password=mariadb.password < initialise.sql
